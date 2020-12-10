9a818718ccb24b7fcb4213dc87390b55fc4dfdf9
Nathaniel Cain <nathaniel2897@gmail.com>
2020-12-10 04:19:26 -0500

    implementing email form to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,26 @@
 import React, { Component, Fragment } from 'react'
-import { Header, Icon, Container } from 'semantic-ui-react'
+import { Header, Icon, Container, Segment, Form, Button, Input, TextArea } from 'semantic-ui-react'
 
 class Contact extends Component {
 
+  state = {
+    email: "",
+    name: "",
+    subject: "",
+    body: ""
+  }
+
   componentDidMount() {
     console.log("contact!")
+  }
+
+  handleSubmit = (e) => {
+    this.setState({
+      email: e.target.email.value,
+      name: e.target.name.value,
+      subject: e.target.subject.value,
+      body: e.target.body.value
+    }, () => console.log(this.state))
   }
 
   render() {
@@ -20,6 +36,27 @@ class Contact extends Component {
         </Fragment>
         <Header as='h2' dividing>
         </Header>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <label>Email</label>
+              <input email="email" placeholder="Email" />
+            </Form.Field>
+            <Form.Field>
+              <label>Name</label>
+              <input name="name" placeholder="Name" />
+            </Form.Field>
+          </Form.Group>
+          <Form.Field>
+            <label>Subject</label>
+            <input subject="subject" placeholder="Subject" />
+          </Form.Field>
+          <Form.Field>
+            <label>Body</label>
+            <input body="body" placeholder="Your email here..." />
+          </Form.Field>
+          <Button type='submit'>Send Email</Button>
+        </Form>
       </Container>
     )
   }
